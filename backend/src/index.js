443941a66b6236e92c20d704d460c55abdbe3930
SJWONG27 <soonjitwong@gmail.com>
2024-05-23 11:52:36 +0800

    missing import

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,48 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
-
+const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
+const authRoute = require("./routes/AuthRoute");
+const { MONGO_URI, PORT } = process.env;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 console.log('PORT:', process.env.PORT);
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
-// Example route
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI, {
-  tls: true,
+
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
   console.log('Connected to MongoDB');
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
 }).catch((error) => {
   console.error('Connection error', error);
 });
 
-mongoose.set('debug', true);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    origin: ['https://localhost:4000'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/",authRoute);
 
