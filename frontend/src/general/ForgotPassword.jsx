d344a5f20c6a7c67cda95e752e5f302dae4e119f
YU HENG GOH <22004880@siswa.um.edu.my>
2024-06-04 19:18:26 +0800

    modify reset password and forget password page

--- frontend/src/general/ForgotPassword.jsx
+++ frontend/src/general/ForgotPassword.jsx
@@ -16,18 +16,27 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="main-container">
-      <h2>Forgot Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-          required
-        />
-        <button type="submit">Send Reset Link</button>
-      </form>
+    <div className="reset-password-page">
+      <div className="main-container">
+        <div className="main-title">
+          <h2>Forgot Password</h2>
+        </div>
+        <form className="form-container" onSubmit={handleSubmit}>
+          <div className="form-input">
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="submit-container">
+            <button type="submit">Sent Reset Link</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/general/ResetPassword.jsx
+++ frontend/src/general/ResetPassword.jsx
@@ -20,19 +20,27 @@ const ResetPassword = () => {
   };
 
   return (
-    <div className="main-container">
-      <h2>Reset Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter your new password"
-          required
-        />
-        <button type="submit">Reset Password</button>
-      </form>
-    </div>
+    <div className="reset-password-page">
+      <div className="main-container">
+        <div className="main-title">
+          <h2>Reset Password</h2>
+        </div>
+        <form className="form-container" onSubmit={handleSubmit}>
+          <div className="form-input">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your new password"
+              required
+            />
+          </div>
+          <div className="submit-container">
+            <button type="submit">Reset Password</button>
+          </div>
+        </form>
+      </div>
+    </div >
   );
 };
 
